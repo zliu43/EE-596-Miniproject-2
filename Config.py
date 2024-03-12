7449111ca50d733e6a0e528d5910c091e9e20b53
zliu43 <136205983+zliu43@users.noreply.github.com>
2024-03-12 03:47:06 -0700

    Update Config.py
    lmao

--- Config.py
+++ Config.py
@@ -1,5 +1,5 @@
 class Config:
-    openai_key = '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'
-    pinecone_key = '7ab07793-704c-4f0b-99cc-28634781f53d'
+    openai_key = None
+    pinecone_key = None
     pinecone_index_name = "ee596-pinecone-index"
     chatty = True
