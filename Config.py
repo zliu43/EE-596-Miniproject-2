c630639d88af3660bb62ac26a540f3cb0f573c1b
zliu43 <136205983+zliu43@users.noreply.github.com>
2024-03-12 14:06:51 -0700

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,7 +1,7 @@
-import os
+import streamlit as st
 
 class Config:
-    openai_key = os.environ.get("OPENAI_KEY")
-    pinecone_key = os.environ.get("PINECONE_KEY")
+    openai_key = st.secrets["OPENAI_KEY"]
+    pinecone_key = st.secrets["PINECONE_KEY"]
     pinecone_index_name = "ee596-pinecone-index"
     chatty = True
