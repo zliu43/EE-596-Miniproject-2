5a3b0afb39e309528c7f5bc2b7f86541be60009d
zliu43 <136205983+zliu43@users.noreply.github.com>
2024-03-12 05:04:15 -0700

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,5 +1,7 @@
+import os
+
 class Config:
-    openai_key = None
-    pinecone_key = None
+    openai_key = os.environ.get("OPENAI_KEY")
+    pinecone_key = os.environ.get("PINECONE_KEY")
     pinecone_index_name = "ee596-pinecone-index"
     chatty = True
